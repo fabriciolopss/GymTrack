11913999de421c010e8258444322ffd2bc588034
fabriciolopss <fabriciolopss@ufmg.br>
2025-05-02 01:37:56 -0300

    hotfix to improve routing

--- codigo/scripts/layout.js
+++ codigo/scripts/layout.js
@@ -7,23 +7,16 @@ class LayoutManager {
     this.addEventListeners();
   }
 
+  getAttributeFromItem(item) {
+    return item.getAttribute("redirect-session-name");
+  }
+
   setActiveHeaderFromURL() {
     const path = window.location.pathname;
-    const fileName = path.split("/").pop();
-
-    let sectionName = "Dashboard";
-
-    if (fileName && fileName !== "index.html" && fileName !== "") {
-      sectionName = fileName.replace(".html", "").replace(/-/g, " ");
-      sectionName = this.capitalizeEachWord(sectionName);
-    }
-
-    this.activeItem = sectionName;
+    this.activeItem = path.split("/").pop().split(".")[0];
 
     this.headerItems.forEach((headerItem) => {
-      if (
-        headerItem.innerText.trim().toLowerCase() === sectionName.toLowerCase()
-      ) {
+      if (this.getAttributeFromItem(headerItem) === this.activeItem) {
         headerItem.classList.add("active");
       } else {
         headerItem.classList.remove("active");
@@ -50,15 +43,13 @@ class LayoutManager {
       headerItem.classList.remove("active");
     });
     item.classList.add("active");
-    this.redirectToSection(item.innerText);
+    this.redirectToSection(item.getAttribute("redirect-session-name"));
   }
 
-  redirectToSection(section) {
-    if (section.toLowerCase() === "dashboard") {
-      window.location.href = `/codigo/index.html`;
-    } else {
-      let fileName = section.toLowerCase().replace(/\s+/g, "-") + ".html";
-      window.location.href = `/codigo/sections/${fileName}`;
-    }
+  redirectToSection(session) {
+    let fileName = session.toLowerCase().replace(/\s+/g, "-") + ".html";
+    window.location.href = `/codigo${
+      session === "index" ? "" : "/sections"
+    }/${fileName}`;
   }
 }
