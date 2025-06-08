36e55cae93a74a3d825de991a60433545777ec38
Gabriel Silva Neiva Amaro <gabrielsn.amaro@gmail.com>
2025-06-08 15:18:25 -0300

    cadastro ficha: criar fichas

--- codigo/scripts/cadastro-treino.js
+++ codigo/scripts/cadastro-treino.js
@@ -11,8 +11,9 @@ const interfaceDados = document.querySelector("#dados-treino");
 const selecionarPlano = document.querySelector("#selecao-plano");
 const customBtn = document.querySelector("#personalizar");
 const criarBtn = document.querySelector("#criar");
-const cancelarBtn = document.querySelector("#cancelar-alteracoes");
+const cancelarBtn = document.querySelector("#cancelar");
 const excluirFicha = document.querySelector("#excluir-ficha");
+const cadastrar = document.querySelector("#concluir-criacao");
 const containerDia1 = document.querySelector("#first-day");
 const containerDia2 = document.querySelector("#second-day");
 
@@ -303,6 +304,234 @@ function carregarPlano() {
   }
 }
 
+    treinoSelect = {
+    "id": (gymApp.edited_trainings[(gymApp.edited_trainings.length)-1].id)+1,
+    "name": "Nova ficha",
+    "category": "",
+    "type": "",
+    "days": [
+      {
+        "id": 1,
+        "xp": 100,
+        "name": "Dia 1 - Nova ficha",
+        "day": []
+      },
+      {
+        "id": 2,
+        "xp": 150,
+        "name": "Dia 2 - Nova ficha",
+        "day": []
+      }
+    ]
+    };
+function criarFicha() {
+  {
+    let botaoExcluirD1 = [];
+    let botaoExcluirD2 = [];
+    let botaoAddD1, botaoAddD2;
+
+    document.querySelector("#container-selecao-plano").style = "display: none";
+
+    indiceTreinoSelect = -1;
+
+    cadastrar.style = "display: inline; margin-top: 2vh";
+    cancelarBtn.style = "display: inline; margin-top: 2vh";
+    criarBtn.style = "display: none";
+    
+
+    interfaceDados.innerHTML = `
+        <label for="titulo-treino" class="mt-2">Título do treino: </label>
+        <input id="titulo-treino" value="${treinoSelect.name}"/>
+        <label for="categoria-treino">Categoria: </label>
+        <input id="categoria-treino" value="${treinoSelect.category}"/>
+        <label for="selecao-nivel">Nível de dificuldade</label>
+        <select id="selecao-nivel">
+            <option hidden>Selecionar...</option>
+            <option value="Ficha iniciante" ${
+              treinoSelect.type == "Ficha iniciante" ? "selected" : ""
+            }>Iniciante</option>
+            <option value="Ficha intermediária" ${
+              treinoSelect.type == "Ficha intermediária" ? "selected" : ""
+            }>Intermedário</option>
+            <option value="Ficha avançada" ${
+              treinoSelect.type == "Ficha avançada" ? "selected" : ""
+            }>Avançado</option>
+        </select>
+    `;
+
+    let containerDias = `
+        <h1 class="mt-3">Dia 1</h1>
+        <label for="titulo-first">Título do dia: </label>
+        <input id="titulo-first" class="mb-3" value="${treinoSelect.days[0].name}"/>
+        <div id="first-day">
+    `;
+    for (i = 0; i < treinoSelect.days[0].day.length; i++) {
+      containerDias =
+        containerDias +
+        `
+            <div class="exercicio d-flex flex-row justify-content-between align-items-center">
+                <div>
+                    <input class="fs-5" type="text" id="dia-1-nome-exercicio-${
+                      i + 1
+                    }" value="${treinoSelect.days[0].day[i].exercise}">
+                    <div id="series-rep">
+                        <label for="dia-1-series-exercicio-${
+                          i + 1
+                        }">Número de séries: </label>
+                        <input type="number" id="dia-1-series-exercicio-${
+                          i + 1
+                        }" value="${treinoSelect.days[0].day[i].series}">
+                        
+                        <label for="dia-1-rep-exercicio-${
+                          i + 1
+                        }">Número de repetições</label>
+                        <select id="dia-1-rep-exercicio-${i + 1}">
+                            <option hidden>Selecionar...</option>
+                            <option value="4-6" ${
+                              treinoSelect.days[0].day[i].repetitions == "4-6"
+                                ? "selected"
+                                : ""
+                            }>4-6</option>
+                            <option value="8-12" ${
+                              treinoSelect.days[0].day[i].repetitions == "8-12"
+                                ? "selected"
+                                : ""
+                            }>8-12</option>
+                            <option value="12-15" ${
+                              treinoSelect.days[0].day[i].repetitions == "12-15"
+                                ? "selected"
+                                : ""
+                            }>12-15</option>
+                        </select>
+                    </div>
+                </div>
+                <button id="delete-ex${i + 1}-d1">Excluir</button>
+            </div>
+        `;
+    }
+
+    containerDias += `
+        </div>
+        <button id="adicionar-exercicio-d1">Adicionar exercício</button>
+        <h1 class="mt-3">Dia 2</h1>
+        <label for="titulo-second">Título do dia: </label>
+        <input id="titulo-second" class="mb-3" value="${treinoSelect.days[1].name}"/>
+        <div id="second-day">
+    `;
+
+    for (i = 0; i < treinoSelect.days[1].day.length; i++) {
+      containerDias =
+        containerDias +
+        `
+            <div class="exercicio d-flex flex-row justify-content-between align-items-center">
+                <div>
+                    <input class="fs-5" type="text" id="dia-2-nome-exercicio-${
+                      i + 1
+                    }" value="${treinoSelect.days[1].day[i].exercise}">
+                    <div id="series-rep">
+                        <label for="dia-2-series-exercicio-${
+                          i + 1
+                        }">Número de séries: </label>
+                        <input type="number" id="dia-2-series-exercicio-${
+                          i + 1
+                        }" value="${treinoSelect.days[1].day[i].series}">
+                        
+                        <label for="dia-2-rep-exercicio-${
+                          i + 1
+                        }">Número de repetições</label>
+                        <select id="dia-2-rep-exercicio-${i + 1}">
+                            <option hidden>Selecionar...</option>
+                            <option value="4-6" ${
+                              treinoSelect.days[1].day[i].repetitions == "4-6"
+                                ? "selected"
+                                : ""
+                            }>4-6</option>
+                            <option value="8-12" ${
+                              treinoSelect.days[1].day[i].repetitions == "8-12"
+                                ? "selected"
+                                : ""
+                            }>8-12</option>
+                            <option value="12-15" ${
+                              treinoSelect.days[1].day[i].repetitions == "12-15"
+                                ? "selected"
+                                : ""
+                            }>12-15</option>
+                        </select>
+                    </div>
+                </div>
+                <button id="delete-ex${i + 1}-d2">Excluir</button>
+            </div>
+        `;
+    }
+    containerDias += `
+        </div>
+        <button id="adicionar-exercicio-d2">Adicionar exercício</button>
+    `;
+    interfaceDados.innerHTML += containerDias;
+
+    botaoAddD1 = document.querySelector("#adicionar-exercicio-d1");
+    botaoAddD2 = document.querySelector("#adicionar-exercicio-d2");
+
+    botaoAddD1.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      treinoSelect.days[0].day[treinoSelect.days[0].day.length] = {
+        exercise: "Novo exercício",
+        series: "",
+        repetitions: "",
+      };
+      criarFicha();
+    });
+
+    botaoAddD2.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      treinoSelect.days[1].day[treinoSelect.days[1].day.length] = {
+        exercise: "Novo exercício",
+        series: "",
+        repetitions: "",
+      };
+      criarFicha();
+    });
+
+    for (let i = 0; i < treinoSelect.days[0].day.length; i++) {
+      botaoExcluirD1[i] = document.querySelector(`#delete-ex${i + 1}-d1`);
+      botaoExcluirD1[i].addEventListener("click", (e) => {
+        e.preventDefault();
+
+        let confirmExcluir = confirm(
+          `Confirmar exclusão de "${treinoSelect.days[0].day[i].exercise}"? Você poderá recuperá-lo se cancelar as alterações.`
+        );
+
+        if (confirmExcluir) {
+          treinoSelect.days[0].day.splice(i, 1);
+          botaoExcluirD1.splice(i, 1);
+
+          carregarPlano();
+        }
+      });
+    }
+
+    for (let i = 0; i < treinoSelect.days[1].day.length; i++) {
+      botaoExcluirD2[i] = document.querySelector(`#delete-ex${i + 1}-d2`);
+      botaoExcluirD2[i].addEventListener("click", (e) => {
+        e.preventDefault();
+
+        let confirmExcluir = confirm(
+          `Confirmar exclusão de "${treinoSelect.days[1].day[i].exercise}"? Você poderá recuperá-lo se cancelar as alterações.`
+        );
+
+        if (confirmExcluir) {
+          treinoSelect.days[1].day.splice(i, 1);
+          botaoExcluirD2.splice(i, 1);
+
+          carregarPlano();
+        }
+      });
+    }
+  }
+}
+
 selecionarPlano.innerHTML = selecionarTemp;
 selecionarPlano.addEventListener("change", (e) => {
   e.preventDefault();
@@ -361,4 +590,18 @@ excluirFicha.addEventListener("click", e => {
     localStorage.setItem("gymAppData", JSON.stringify(gymApp));
     location.reload();
   }
+});
+
+criarBtn.addEventListener("click", e => {
+  e.preventDefault();
+
+  criarFicha();
+});
+
+cadastrar.addEventListener("click", e => {
+  e.preventDefault();
+
+  gymApp.edited_trainings[gymApp.edited_trainings.length] = treinoSelect;
+  localStorage.setItem("gymAppData", JSON.stringify(gymApp));
+  location.reload();
 });
